9c4fa86d81365581cf2cc74434fe2cd0e03fff80
Matt Bremer <mjb2@illinois.edu>
2019-04-23 16:44:59 -0500

    Added control interface module

---
+++ soc/synthesis/submodules/control_interface.sv
@@ -0,0 +1,98 @@
+/************************************************************************
+Avalon-MM Interface for AES Decryption IP Core
+
+Dong Kai Wang, Fall 2017
+
+For use with ECE 385 Experiment 9
+University of Illinois ECE Department
+
+Register Map:
+
+ 0-3 : 4x 32bit AES Key
+ 4-7 : 4x 32bit AES Encrypted Message
+ 8-11: 4x 32bit AES Decrypted Message
+   12: Not Used
+	13: Not Used
+   14: 32bit Start Register
+   15: 32bit Done Register
+
+************************************************************************/
+
+module control_interface (
+	// Avalon Clock Input
+	input logic CLK,
+	
+	// Avalon Reset Input
+	input logic RESET,
+	output logic [15:0]ATTACK,RLEASE,SUSTAIN,DECAY,
+	output logic [1:0] SHAPE1,SHAPE0,
+	output logic [6:0] FREQ0, FREQ1, FREQ2, FREQ3,
+	output logic [15:0] AMP1_0, AMP0_0, AMP1_1, AMP0_1, AMP1_2, AMP0_2, AMP1_3, AMP0_3,
+	output logic KEY3, KEY2, KEY1, KEY0,
+	
+	input logic [5:0] AVL_ADDR,
+	input logic [3:0] AVL_BYTE_EN,
+	input logic AVL_READ, AVL_WRITE, AVL_CS,
+	input logic [31:0] AVL_WRITEDATA,
+	output logic [31:0] AVL_READDATA
+);
+
+   logic [31:0] reg_file [0:63];
+	//logic [31:0] reg_file_next [0:63];
+	
+	
+	assign SHAPE0 = reg_file[0][1:0];
+	assign SHAPE1 = reg_file[1][1:0];
+	assign ATTACK = reg_file[2][15:0];
+	assign DECAY = reg_file[3][15:0];
+	assign SUSTAIN = reg_file[4][15:0];
+	assign RLEASE = reg_file[5][15:0];
+	
+	assign KEY0 = reg_file[32][0];
+	assign KEY1 = reg_file[33][0];
+	assign KEY2 = reg_file[34][0];
+	assign KEY3 = reg_file[35][0];
+	
+	assign FREQ0 = reg_file[40][6:0];
+	assign FREQ1 = reg_file[41][6:0];
+	assign FREQ2 = reg_file[42][6:0];
+	assign FREQ3 = reg_file[43][6:0];
+	
+	assign AMP1_0 = reg_file[48][15:0];
+	assign AMP1_1 = reg_file[49][15:0];
+	assign AMP1_2 = reg_file[50][15:0];
+	assign AMP1_3 = reg_file[51][15:0];
+	
+	assign AMP0_0 = reg_file[56][15:0];
+	assign AMP0_1 = reg_file[57][15:0];
+	assign AMP0_2 = reg_file[58][15:0];
+	assign AMP0_3 = reg_file[59][15:0];
+	
+	
+	// Select Regs to output from regfile
+always_comb
+begin
+	if (AVL_READ & AVL_CS)
+		AVL_READDATA = reg_file[AVL_ADDR];
+	else
+		AVL_READDATA = 32'dx;
+end
+
+	
+	always_ff @ (posedge CLK)
+	begin
+		if (RESET)
+		begin
+			for (integer i=0; i<64; i=i+1) begin
+				reg_file[i] <= 0;
+			end
+		end
+		else if (AVL_WRITE & AVL_CS)
+			begin
+				reg_file[AVL_ADDR] = AVL_WRITEDATA;
+			end
+		
+		
+	end
+	
+endmodule
--- software/synth/MIDI.c
+++ software/synth/MIDI.c
@@ -7,6 +7,8 @@
 
 #include "MIDI.h"
 
+static alt_u32* const controls = SYNTH_CONTROLLER_0_BASE;
+
 static char * CIN_names[] = {"Miscellaneous Function Code",
                             "Cable Event",
                             "2-Byte System Common Message",
@@ -26,37 +28,29 @@ static char * CIN_names[] = {"Miscellaneous Function Code",
 
 void initControls()
 {
-	alt_u8* const freq = FREQ0_BASE;
-	alt_u16* const amp = AMP0_BASE;
-	alt_u8* const key_on =  KEY_ON_BASE;
-	alt_u8* const shape = SHAPESEL_BASE;
-	alt_u16* const attack = ATTACK_BASE;
-	alt_u16* const decay = DECAY_BASE;
-	alt_u16* const sustain = SUSTAIN_BASE;
-	alt_u16* const release = RELEASE0_BASE;
-
-
-	*freq = 0;
-	*amp = 0;
-	*key_on = 0;
-	*shape = 0;
-	*attack = 0x7FFF;
-	*decay = 0x7FFF;
-	*sustain = 0x7FFF;
-	*release = 0x7FFF;
+//	alt_u8* const freq = FREQ0_BASE;
+//	alt_u16* const amp = AMP0_BASE;
+//	alt_u8* const key_on =  KEY_ON_BASE;
+//	alt_u8* const shape = SHAPESEL_BASE;
+//	alt_u16* const attack = ATTACK_BASE;
+//	alt_u16* const decay = DECAY_BASE;
+//	alt_u16* const sustain = SUSTAIN_BASE;
+//	alt_u16* const release = RELEASE0_BASE;
+//
+//
+//	*freq = 0;
+//	*amp = 0;
+//	*key_on = 0;
+//	*shape = 0;
+//	*attack = 0x7FFF;
+//	*decay = 0x7FFF;
+//	*sustain = 0x7FFF;
+//	*release = 0x7FFF;
 }
 
 void ProcessMIDIPacket(alt_u32 packet)
 {
-	//alt_u8* const hex = KEYCODE_BASE;
-	alt_u8* const freq = FREQ0_BASE;
-	alt_u16* const amp = AMP0_BASE;
-	alt_u8* const key_on =  KEY_ON_BASE;
-	alt_u8* const shape = SHAPESEL_BASE;
-	alt_u16* const attack = ATTACK_BASE;
-	alt_u16* const decay = DECAY_BASE;
-	alt_u16* const sustain = SUSTAIN_BASE;
-	alt_u16* const release = RELEASE0_BASE;
+
     alt_u8 bytes[4];
     for (int i = 0; i < 4; i ++) {
         //bytes[i] = (packet >> (8*(3-i))) & 0xff;
@@ -69,57 +63,56 @@ void ProcessMIDIPacket(alt_u32 packet)
     alt_u8 CIN = bytes[0];
     alt_u8 note = bytes[2];
     alt_u8 velocity = bytes[3];
-    alt_u8 control = bytes[2];
-    long ADSR;
+//    alt_u8 control = bytes[2];
+//    long ADSR;
 
     switch(CIN) {
         case NOTE_ON:
-        	*key_on = 0x00;
-    		*freq = (alt_u8)note;
+        	controls[7] = 0x01;
+    		controls[8] = (alt_u32)note;
     		//*hex = note;
-    		*amp = (alt_u16)velocity << 8;
-    		*key_on = 0x01;
+    		controls[9] = (alt_u32)velocity << 8;
             break;
         case NOTE_OFF:
-        	if (*freq == note) {
+        	if (controls[8] == (alt_u32)note) {
         		//*hex = 0;
-        		*key_on = 0x00;
+        		controls[7] = 0x00;
         	}
         	break;
-        case CONTROL_CHANGE:
-        	if (control == 0x18) {
-        		if (velocity == 0)
-        			*shape = *shape & ~0x01; // Clear the first bit
-        		else
-        			*shape = *shape | 0x01; // Set the first bit
-        	} else if (control == 0x19) {
-        		if (velocity == 0)
-        			*shape = *shape & ~0x02; // Clear the second bit
-        		else
-        			*shape = *shape | 0x02; // Set the second bit
-        	}else if (control == 0x1a) {
-        		if (velocity == 0)
-        		    *shape = *shape & ~0x04; // Clear the third bit
-        		else
-        		    *shape = *shape | 0x04; // Set the third bit
-        	}else if (control == 0x1b) {
-        		if (velocity == 0)
-        		    *shape = *shape & ~0x08; // Clear the fourth bit
-        		else
-        		    *shape = *shape | 0x08; // Set the fourth bit
-        	} else if (control == 0x01){ //attack
-        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
-        		*attack = ADSR;
-        	} else if (control == 0x02){ //decay
-        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
-        		*decay = ADSR;
-        	} else if (control == 0x03){ //sustain
-        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
-        		*sustain = ADSR;
-        	} else if (control == 0x04){ //release
-        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
-        		*release = ADSR;
-        	}
+//        case CONTROL_CHANGE:
+//        	if (control == 0x18) {
+//        		if (velocity == 0)
+//        			*shape = *shape & ~0x01; // Clear the first bit
+//        		else
+//        			*shape = *shape | 0x01; // Set the first bit
+//        	} else if (control == 0x19) {
+//        		if (velocity == 0)
+//        			*shape = *shape & ~0x02; // Clear the second bit
+//        		else
+//        			*shape = *shape | 0x02; // Set the second bit
+//        	}else if (control == 0x1a) {
+//        		if (velocity == 0)
+//        		    *shape = *shape & ~0x04; // Clear the third bit
+//        		else
+//        		    *shape = *shape | 0x04; // Set the third bit
+//        	}else if (control == 0x1b) {
+//        		if (velocity == 0)
+//        		    *shape = *shape & ~0x08; // Clear the fourth bit
+//        		else
+//        		    *shape = *shape | 0x08; // Set the fourth bit
+//        	} else if (control == 0x01){ //attack
+//        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
+//        		*attack = ADSR;
+//        	} else if (control == 0x02){ //decay
+//        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
+//        		*decay = ADSR;
+//        	} else if (control == 0x03){ //sustain
+//        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
+//        		*sustain = ADSR;
+//        	} else if (control == 0x04){ //release
+//        		ADSR = map((alt_u16)velocity, 0, 0x007F, 0, 0x7FFF);
+//        		*release = ADSR;
+//        	}
 
 
 
