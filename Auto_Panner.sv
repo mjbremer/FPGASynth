4fbf3396017796f2bb65831de83b1ef4a2bc0555
Justin Meyer <jlmeyer4@illinois.edu>
2019-04-30 10:05:51 -0500

    i wanna pull

--- Auto_Panner.sv
+++ Auto_Panner.sv
@@ -11,7 +11,7 @@ Integrator int0 	(.Clk(CLK),
 						.CLOCK_50(CLOCK_50),
 						.Reset(RESET),
 						.loadF(1'b1),
-						.F_in(24'h000080),
+						.F_in(PANNER),
 						.out(addr)
 						);
 		
@@ -20,7 +20,8 @@ rom #("sine.mem", 12,16) sine (.Clk(CLK),
 										 .CS(1'b1),
 										 .addr(addr),
 										 .data(sine_out));
-										 
+		
+//24'h000080		
 			
 always_comb
 begin		
@@ -28,7 +29,7 @@ begin
 	math = ($signed(sine_out) >>> 1) + 16'h4000; //this makes a sine wave from x7FFF to x0000
 
 		case(AUTO_PAN_EN)
-			1'b0: PAN_OUT = PANNER;
+			1'b0: PAN_OUT = 16'h4000;
 			1'b1:	PAN_OUT = math[15:0];
 		endcase
 end
